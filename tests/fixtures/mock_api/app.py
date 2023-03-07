2cc330ff7f101cb5f8e841a5f23aeffd2d84e265
Thomas J. Zajac <thomas-jakob.zajac@uni-tuebingen.de>
2023-03-07 11:19:34 +0000

    Bumped version, removed TODOs

--- tests/fixtures/mock_api/app.py
+++ tests/fixtures/mock_api/app.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+"""Mock EKSS endpoints"""
 
 from fastapi import FastAPI, Request, status
 from fastapi.responses import JSONResponse, Response
